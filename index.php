1567355e43b83810a3100995926a206ddc600031
foodboyhp <hoanghieppham8b9@gmail.com>
2023-10-24 01:52:21 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ex2</title>
+</head>
+<body>
+    <main>
+        <form method="post" action="generate.php">
+            <h1>Nhập thông tin sinh viên</h1>
+            Họ tên: <input type="text" name="name"><br>
+            MSSV: <input type="text" name="mssv"><br>
+            <input type="submit" value="Xác nhận">
+        </form>
+    </main>
+</body>
+</html>
